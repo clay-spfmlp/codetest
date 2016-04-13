d1a99598967b790f1e563a7655240ef52e42a24a
Clay Malven <claymalven@Clay.local>
2016-04-12 23:54:06 -0500

    Add the visual representation code back to better understand my process, ran spell check

--- EquilibriumIndex.php
+++ EquilibriumIndex.php
@@ -1,7 +1,8 @@
 <?php
 
+
 /**
- * Returns an array of equilibrium indexs from the array provided.
+ * Returns an array of equilibrium indexes from a sequential array.
  * 
  * @param  Array $arr
  * @return Array $output
@@ -11,36 +12,62 @@ function getEquilibriums(Array $arr) {
 	# Logic goes here!
 	// loop through $arr
 	for($i = 0; $i < count($arr); $i++) { 
-		// set an array for the left side. we do this so we dont 
-		// chage the origanl array when we use array_splice.
+		// set an array for the left side. we do this so we don't 
+		// change the original array when we use array_splice.
 		$left = $arr;
 
-		//grab everything for the left side of the index and add togther.
+		//grab everything for the left side of the index and add together.
 		array_splice($left, $i);
 		$leftSum = array_sum($left);
 
-		// grab everything for the right side of the index and add togther. 
+		// grab everything for the right side of the index and add together. 
 		// notice the +1. this is to avoid adding itself to the array
-		$rightSum = array_sum(array_slice($arr, $i+1));
+		// I also could of combine these two line together but wanted to 
+		// keep the $right variable to use below to test. 
+		$right = array_slice($arr, $i+1);
+		$rightSum = array_sum($right);
 
 		// last we check if the two sums are equal and if they are we add 
 		// it to the $output array.
-		if($leftSum === $rightSum) $output[] = $i; 
+		if($leftSum === $rightSum) $output[] = $i;
+
+		// I left this in to show a visual representation of the process. 
+		// uncomment below to view.
+		// dd($left);
+		// dd('Left Sum :'.$leftSum);
+		// dd($right);
+		// dd('Right Sum :'.$rightSum);
+		// dd('===============');
+
 	}
 	// and don't forget to return the output.
 	return $output; 
 }
- 
-print_r(getEquilibriums(array(-7, 1, 5, 2, -4, 3, 0)));
 
 /**
- * So when I googled equilibrium indexes I came across this code, 
- * not wanting to cheat I deceid I would come up with my own, but 
+ *	Helper function to dump out values in a more readable way. 
+ * 
+ * @param  Any $value
+ */
+function dd($value){
+	print '<pre>';
+	print_r($value);
+	print '</pre>';
+}
+
+dd(getEquilibriums(array(-7, 1, 5, 2, -4, 3, 0)));
+
+
+
+
+/**
+ * So when I goggled equilibrium indexes I came across this code, 
+ * not wanting to cheat I deiced I would come up with my own, but 
  * after I was done I wanted to come back and look at the code I 
- * found online. This code is a little better and I just wanted to 
+ * found on-line. This code is a little better and I just wanted to 
  * go over it and add my own comments to explain how this works.
  *
- * The best thing about this code is that it dosen't call any php 
+ * The best thing about this code is that it doesn't call any php 
  * function inside the foreach loop which should be a big boost in 
  * performance.
  *
@@ -51,6 +78,7 @@ print_r(getEquilibriums(array(-7, 1, 5, 2, -4, 3, 0)));
  * @return Array $equilibriums
  */
 function getEquilibriumsFoundOnline($arr) {
+
 	// get the sum of the array right off the back.
     $right = array_sum($arr);  
     // set $left to 0 because at first there is nothing to the left.
